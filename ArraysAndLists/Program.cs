223fceb1b0338310b6173153f270ef83922057d2
gamejunkiez <queenangrylobster@gmail.com>
2022-06-12 23:17:04 -0500

    completed exercise

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -6,15 +6,18 @@ namespace ArraysAndLists
     {
         static void Main(string[] args)
         {
+            var oddCount = 0;
+            var evenCount = 0;
+
             //TODO:
 
             // Create an int Array and populate numbers 1-10
-
-            /* Create two Lists of type int.
-             * Name one List "evens"
-             * Name the other List "odds"
-             */
-
+            var oneToTen = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            // Create two Lists of type int.
+            //  Name one List "evens"
+            var evens = new int[5];
+            // Name the other List "odds"
+            var odds = new int[5];
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
              *  if a number is even or odd.
@@ -22,6 +25,32 @@ namespace ArraysAndLists
              * or the odds List
              */
 
+            foreach (int numbers in oneToTen) 
+            {
+                if (numbers % 2 == 0)
+                {
+                    Console.WriteLine($"{numbers} is even! Adding it to the evens array.");
+                    evens[evenCount] = numbers;
+                    evenCount++;
+                }
+                else
+                {
+                    Console.WriteLine($"{numbers} is odd! Adding it to the odds array.");
+                    odds[oddCount] = numbers;
+                    oddCount++;
+                }
+            }
+
+            Console.WriteLine("Displaying even numbers!");
+            for (int i = 0; i < evens.Length; i++) 
+            { 
+                Console.WriteLine(evens[i]); 
+            }
+            Console.WriteLine("Displaying odd numbers!");
+            for (int i = 0; i < odds.Length; i++)
+            {
+                Console.WriteLine(odds[i]);
+            }
             /* Now using foeach or for loops,
              * display each List of even and odd numbers
              *
